2e42a40c21c19ef9a3fbd36b902607e73e9fb51e
teddy wambua <teddywambua5456@gmail.com>
2023-03-13 12:40:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//Test 1
+
+function introduction(name){
+    return (`Hi, my name is ${name}.`);
+}
+
+
+//Test 2
+
+function introductionWithLanguage(name,language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+
+//Test 3
+
+function introductionWithLanguageOptional(name,language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
